474838a405f4a33955156d059f9d7b05ceb0af3c
Kaustubh Kulkarni <57136442+kaustubhk24@users.noreply.github.com>
2021-04-19 23:23:25 +0530

    check description
    Video autoplay done menu created floating functionality remaining

---
+++ src/componenets/MenuComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function MenuComponent() {
+    return (
+        <div id="mydiv">
+  <div id="mydivheader">Click here to move</div>
+  <p>Move</p>
+  <p>this</p>
+  <p>DIV</p>
+</div>
+    )
+}
--- src/componenets/VideoComponent.js
+++ src/componenets/VideoComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function VideoComponent(props) {
     return (
         <div>
-            <video autoPlay id="main-video" src={props.videoData['video-url']}></video>
+            <video autoPlay muted loop  id="main-video" src={props.videoData['video-url']}></video>
         </div>
         
     )
